3571ceb05bf9a900af8e2bd18841d14f43b716f4
Kappo <viktordemychev@gmail.com>
2019-07-27 19:27:06 +0200

    v0.5

--- Webmaps/webmaps.py
+++ Webmaps/webmaps.py
@@ -1,1 +1,11 @@
-312
+import folium
+import pandas   
+
+data = pandas.read_csv("Volcanoes.txt")
+
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
+map = folium.Map(location = [50, -50])
+
+fg = folium.FeatureGroup(name = "MyGroup")
